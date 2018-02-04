0b8645e88bb97bb16b738e691471ad62c93cf8c4
lyupan <lv.steven.pan@gmail.com>
2018-02-04 17:11:48 -0600

    Add basic dependencies

---
+++ src/main/java/com/zappos/lyu/OnlineFoodOrderingApp.java
@@ -0,0 +1,14 @@
+package com.zappos.lyu;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineFoodOrderingApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnlineFoodOrderingApp.class, args);
+    }
+
+}
---
+++ src/main/java/com/zappos/lyu/rest/OnlineFoodOrderingRestController.java
@@ -0,0 +1,14 @@
+package com.zappos.lyu.rest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class OnlineFoodOrderingRestController {
+
+    @GetMapping("/")
+    public String test() {
+        return "Hello World!";
+    }
+}
